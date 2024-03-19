b124f63f268c158771e0176c3b5e594c4a8bb41a
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-03-19 09:01:35 -0400

    Ajout de CaserneDTO et de CaserneModel

---
+++ ProjetPompier_API/Logics/DAOs/CaserneDTO.cs
@@ -0,0 +1,74 @@
+﻿using ProjetPompier_API.Logics.Modeles;
+using System.Text.RegularExpressions;
+
+namespace ProjetPompier_API.Logics.DAOs
+{
+    public class CaserneDTO
+    {
+        #region Proprietes
+        /// <summary>
+        /// Propriété représentant le nom d'une caserne.
+        /// </summary>
+        public string Nom { get; set; }
+
+        /// <summary>
+        /// Propriété représentant l'adresse d'une caserne.
+        /// </summary>
+        public string Adresse { get; set; }
+
+        /// <summary>
+        /// Propriété représentant la ville d'une caserne.
+        /// </summary>
+        public string Ville { get; set; }
+
+        /// <summary>
+        /// Propriété représentant la province d'une caserne.
+        /// </summary>
+        public string Province { get; set; }
+
+        /// <summary>
+        /// Propriété représenant le téléphone d'une caserne.
+        /// </summary>
+        public string Telephone { get; set; }
+
+        #endregion Proprietes
+
+        #region Constructeurs
+        /// <summary>
+        /// Constructeur vide.
+        /// </summary>
+        public CaserneDTO() { }
+
+        /// <summary>
+        /// Constructeur avec paramètres.
+        /// </summary>
+        /// <param name="nom">Nom d'une caserne.</param>
+        /// <param name="adresse">Adresse d'une caserne.</param>
+        /// <param name="ville">Ville d'une caserne.</param>
+        /// <param name="province">Province d'une caserne.</param>
+        /// <param name="telephone">Téléphone d'une caserne.</param>
+        public CaserneDTO(string nom = "", string adresse = "", string ville = "", string province = "", string telephone = "")
+        {
+            Nom = nom;
+            Adresse = adresse;
+            Ville = ville;
+            Province = province;
+            Telephone = telephone;
+        }
+
+        ///// <summary>
+        ///// Constructeur avec le modèle CaserneModel en paramètre.
+        ///// </summary>
+        ///// <param name="laCaserne">L'objet du modèle CaserneModel.</param>
+        //public CaserneDTO(CaserneModel laCaserne)
+        //{
+        //    Nom = laCaserne.Nom;
+        //    Adresse = laCaserne.Adresse;
+        //    Ville = laCaserne.Ville;
+        //    Province = laCaserne.Province;
+        //    Telephone = laCaserne.Telephone;
+        //}
+
+        #endregion Constructeurs
+    }
+}
---
+++ ProjetPompier_API/Logics/Models/CaserneModel.cs
@@ -0,0 +1,32 @@
+﻿namespace ProjetPompier_API.Logics.Modeles
+{
+    public class CaserneModel
+    {
+        #region AttributsProprietes
+
+        private string nom;
+        private string adresse;
+        private string ville;
+        private string province;
+        private string telephone;
+
+        #endregion AttributsProprietes
+
+        #region Constructeurs
+
+        /// <summary>
+        /// Constructeur paramétré
+        /// </summary>
+        /// <param name="unNom">Le nom d'une caserne</param>
+        /// <param name="uneAdresse">L'adresse d'une caserne</param>
+        /// <param name="uneVille">La ville d'une caserne</param>
+        /// <param name="uneProvince">La province d'une caserne</param>
+        /// <param name="unTelephone">Le téléphone d'une caserne</param>
+        public CaserneModel(string unNom = "", string uneAdresse = "", string uneVille = "", string uneProvince = "", string unTelephone = "")
+        {
+            
+        }
+
+        #endregion Constructeurs
+    }
+}
