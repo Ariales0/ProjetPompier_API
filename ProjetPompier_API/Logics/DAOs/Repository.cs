b37eb11bcdcec351726382eea06c9e1cdcb7aaa9
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-03-19 09:08:58 -0400

    Installation du package NuGet System.Data.SqlClient Ecriture du fichier Repository.cs

---
+++ ProjetPompier_API/Logics/DAOs/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.SqlClient;
+
+/// <summary>
+/// Namespace pour les classe de type DAO.
+/// </summary>
+namespace TPCegepAPI.Logics.DAOs
+{
+    /// <summary>
+    /// Classe représentant un repository.
+    /// </summary>
+    public class Repository
+    {
+        #region AttributsPropriete
+
+        /// <summary>
+        /// La connexion.
+        /// </summary>
+        protected SqlConnection connexion;
+
+        #endregion AttributsPropriete
+
+        #region Constructeurs
+        /// <summary>
+        /// Constructeur de la classe.
+        /// </summary>
+        protected Repository()
+        {
+            connexion = new SqlConnection("Server=127.0.0.1; Database=ProjetPompier; User Id=quentin; Password=password;");
+        }
+
+        #endregion Constructeurs
+
+        #region MethodesService
+
+        /// <summary>
+        /// Méthode permettant d'ouvrir la connexion.
+        /// </summary>
+        protected void OuvrirConnexion()
+        {
+            connexion.Open();
+        }
+
+        /// <summary>
+        /// Méthode permettant de fermer la connexion.
+        /// </summary>
+        protected void FermerConnexion()
+        {
+            connexion.Close();
+        }
+
+        #endregion MethodesService
+    }
+}
