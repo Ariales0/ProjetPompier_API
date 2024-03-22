3c570a58b24c5da7eabab0e726d6eeb3fdcc0a97
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-03-22 09:23:28 -0400

    Création du controleur CaserneController (vide). Suppréssion du controleur WeatherForecast et de sa classe.

---
+++ ProjetPompier_API/Controllers/CaserneController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetPompier_API.Controllers
+{
+    public class CaserneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
