3d26e47fd3d9540a2a05305feff5f66231f1a483
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-03-22 09:20:43 -0400

    Ajout des proprietes Nom, Adresse, Ville, Province, Telephone dans la classe CaserneModel

--- ProjetPompier_API/Logics/Modeles/CaserneModel.cs
+++ ProjetPompier_API/Logics/Modeles/CaserneModel.cs
@@ -16,9 +16,39 @@ namespace ProjetPompier_API.Logics.Modeles
         private string nom;
 
         /// <summary>
+        /// Propriété représentant le nom de la Caserne.
+        /// </summary>
+        public string Nom
+        {
+            get { return nom; }
+            set
+            {
+                if (value.Length <= 50)
+                    nom = value;
+                else
+                    throw new Exception("Le nom de la Caserne doit avoir un maximum de 50 caractères.");
+            }
+        }
+
+        /// <summary>
         /// Attribut représentant le adresse de la caserne.
         /// </summary>
         private string adresse;
+
+        /// <summary>
+        /// Propriété représentant l'adresse de la Caserne.
+        /// </summary>
+        public string Adresse
+        {
+            get { return adresse; }
+            set
+            {
+                if (value.Length <= 100)
+                    nom = value;
+                else
+                    throw new Exception("L'adresse de la Caserne doit avoir un maximum de 100 caractères.");
+            }
+        }
 
         /// <summary>
         /// Attribut représentant le ville de la caserne.
@@ -26,14 +56,59 @@ namespace ProjetPompier_API.Logics.Modeles
         private string ville;
 
         /// <summary>
+        /// Propriété représentant la ville de la Caserne.
+        /// </summary>
+        public string Ville
+        {
+            get { return ville; }
+            set
+            {
+                if (value.Length <= 75)
+                    nom = value;
+                else
+                    throw new Exception("La ville de la Caserne doit avoir un maximum de 75 caractères.");
+            }
+        }
+
+        /// <summary>
         /// Attribut représentant le province de la caserne.
         /// </summary>
         private string province;
 
         /// <summary>
+        /// Propriété représentant la province de la Caserne.
+        /// </summary>
+        public string Province
+        {
+            get { return province; }
+            set
+            {
+                if (value.Length <= 50)
+                    nom = value;
+                else
+                    throw new Exception("La province de la Caserne doit avoir un maximum de 50 caractères.");
+            }
+        }
+
+        /// <summary>
         /// Attribut représentant le telephone de la caserne.
         /// </summary>
         private string telephone;
+
+        /// <summary>
+        /// Propriété représentant le telephone de la Caserne.
+        /// </summary>
+        public string Telephone
+        {
+            get { return telephone; }
+            set
+            {
+                if (value.Length <= 12)
+                    nom = value;
+                else
+                    throw new Exception("Le telephone de la Caserne doit avoir un maximum de 50 caractères.");
+            }
+        }
 
         #endregion AttributsProprietes
 
@@ -49,7 +124,11 @@ namespace ProjetPompier_API.Logics.Modeles
         /// <param name="unTelephone">Le téléphone d'une caserne</param>
         public CaserneModel(string unNom = "", string uneAdresse = "", string uneVille = "", string uneProvince = "", string unTelephone = "")
         {
-            
+            Nom = unNom;
+            Adresse = uneAdresse;
+            Ville = uneVille;
+            Province = uneProvince;
+            Telephone = unTelephone;
         }
 
         #endregion Constructeurs
