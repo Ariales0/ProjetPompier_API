86996dc616b53ebf54815745b9014a973d59558b
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-05-03 19:52:31 -0400

    Patron sigleton pour TypesInterventionRepository

--- ProjetPompier_API/Logics/DAOs/TypesInterventionDAO.cs
+++ ProjetPompier_API/Logics/DAOs/TypesInterventionDAO.cs
@@ -1,6 +1,41 @@
-﻿namespace ProjetPompier_API.Logics.DAOs
+﻿using System.Data.SqlClient;
+using System.Data;
+using ProjetPompier_API.Logics.DTOs;
+using ProjetPompier_API.Logics.Exceptions;
+
+
+/// <summary>
+/// Namespace pour les classe de type DAO.
+/// </summary>
+namespace ProjetPompier_API.Logics.DAOs
 {
-    public class TypesInterventionDAO
+    public class TypesInterventionRepository : Repository
     {
+        #region AttributsProprietes
+
+        /// <summary>
+        /// Instance unique du repository.
+        /// </summary>
+        private static TypesInterventionRepository instance;
+
+        /// <summary>
+        /// Propriété permettant d'accèder à l'instance unique de la classe.
+        /// </summary>
+        public static TypesInterventionRepository Instance
+        {
+            get
+            {
+                //Si l'instance est null...
+                if (instance == null)
+                {
+                    //... on crée l'instance unique...
+                    instance = new TypesInterventionRepository();
+                }
+                //...on retourne l'instance unique.
+                return instance;
+            }
+        }
+
+        #endregion AttributsProprietes
     }
 }
