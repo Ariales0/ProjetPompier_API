179c52ae51467a09a78be20e97ff0d285d820050
Quentin Lecourt <quentin.lecourt@protonmail.com>
2024-05-06 18:49:49 -0400

    Commencement de l'ecriture de la methode ObtenirListeEquipe

--- ProjetPompier_API/Logics/DAOs/EquipeDAO.cs
+++ ProjetPompier_API/Logics/DAOs/EquipeDAO.cs
@@ -1,4 +1,9 @@
-﻿/// <summary>
+﻿using System.Data.SqlClient;
+using System.Data;
+
+using ProjetPompier_API.Logics.DTOs;
+
+/// <summary>
 /// Namespace pour les classe de type DAO.
 /// </summary>
 namespace ProjetPompier_API.Logics.DAOs
@@ -6,7 +11,7 @@ namespace ProjetPompier_API.Logics.DAOs
     /// <summary>
     /// Classe DAO d'une equipe.
     /// </summary>
-    public class EquipeRepository
+    public class EquipeRepository : Repository
     {
         #region AttributsProprietes
 
@@ -42,6 +47,43 @@ namespace ProjetPompier_API.Logics.DAOs
         /// </summary>
         private EquipeRepository() : base() { }
 
+        #endregion
+
+        #region MethodesService
+
+        /// <summary>
+        /// Méthode de service permettant d'obtenir la liste des equipes d'une intevention.
+        /// </summary>
+        /// <returns>Liste des equipes d'une intervention.</returns>
+        public List<EquipeDTO> ObtenirListeEquipe()
+        {
+            SqlCommand command = new SqlCommand(" SELECT T_Equipes.Code, " +
+                                                        "T_Pompiers.Matricule," +
+                                                        "T_Grades.Description," +
+                                                        "T_Pompiers.Nom," +
+                                                        "T_Pompiers.Prenom," +
+                                                        "T_Vehicules.Vin  " +
+
+                                                        "FROM T_Equipes " +
+                                                        "INNER JOIN T_FichesIntervention ON T_Equipes.IdIntervention = T_FichesIntervention.IdFicheIntervention " +
+                                                        "INNER JOIN T_Pompiers ON T_Equipes.IdPompier = T_Pompiers.IdPompier " +
+                                                        "INNER JOIN T_Grades ON T_Pompiers.IdGrade = T_Grades.IdGrade " +
+                                                        "INNER JOIN T_Vehicules ON T_Equipes.IdVehicule=T_Vehicules.IdVehicule " +
+                                                        "INNER JOIN T_Casernes ON T_FichesIntervention.IdCaserne=T_Casernes.IdCaserne " +
+
+
+                                                        " WHERE T_Casernes.Nom = @nomCaserne " +
+                                                        "AND T_FichesIntervention.IdPompier = (SELECT IdPompier FROM T_Pompiers WHERE Matricule = @matriculeCapitaine) " +
+                                                        "AND T_FichesIntervention.DateDebut = @dateDebutIntervention " +
+                                                        "GROUP BY T_Equipes.Code, " +
+                                                        "T_Pompiers.Matricule, " +
+                                                        "T_Grades.Description, T_Pompiers.Nom, " +
+                                                        "T_Pompiers.Prenom, T_Vehicules.Vin ", connexion);
+
+            SqlParameter matriculeParam = new SqlParameter("@matriculeCapitaine", SqlDbType.Int, 6);
+            SqlParameter nomCaserneParam = new SqlParameter("@nomCaserne", SqlDbType.VarChar, 100);
+            SqlParameter dateDebutInterventionParam = new SqlParameter("@dateDebutIntervention", SqlDbType.DateTime);
+        }
         #endregion
 
     }
